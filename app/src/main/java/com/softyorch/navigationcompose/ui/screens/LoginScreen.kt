857bf497c645ba09a98564817bc378c14601db15
Jorge1 <agullojorge@gmail.com>
2024-11-17 09:20:21 +0100

    Implemented basic intern navigation with deepLinks

--- app/src/main/java/com/softyorch/navigationcompose/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/softyorch/navigationcompose/ui/screens/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.softyorch.navigationcompose.ui.screens
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,16 +9,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
+import com.softyorch.navigationcompose.ui.core.navigation.URI_HOME
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier, navigateToHome: () -> Unit) {
+    val context = LocalContext.current
+
     Column (modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(modifier = modifier.weight(1f))
         Text(text = "LOGIN SCREEN", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
         Spacer(modifier = modifier.weight(1f))
-        Button(onClick = { navigateToHome() }) {
+        Button(onClick = {
+            //navigateToHome()
+            val intent = Intent(Intent.ACTION_VIEW, URI_HOME.toUri())
+            context.startActivity(intent)
+        }) {
             Text(text = "Go To Home")
         }
         Spacer(modifier = modifier.weight(1f))
